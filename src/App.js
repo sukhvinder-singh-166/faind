4eeaf863837129ebe782812bb49595bc8cced368
sukhvinder-singh-166 <monudhaka829@gmail.com>
2025-03-29 15:45:49 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { useEffect } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <p className="text-[200px] text-red-600">faind</p>
+    </>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+  
+    extend: {
+      colors: {
+        'orange-yellow': '#fff000',
+        'orange-red': '#FF1100',
+      },
+      fontSize: {
+        '3xl': '28px',
+        '4xl': '32px',
+        '5xl': '36px',
+      },
+    },
+  },
+  plugins: [ ],
+}
